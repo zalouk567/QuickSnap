630e27fb8cba98acc4ae396d82e177111664e6cb
BraydanOwen <braydanowen@live.com>
2016-03-11 13:31:37 +1100

    add random shuffle

--- src/GameLogic/Deck.cs
+++ src/GameLogic/Deck.cs
@@ -32,6 +32,7 @@ namespace CardGames.GameLogic
 		            i++;
 		        }    
 		    }
+            Shuffle();
 		
 		    _topCard = 0;
 		}
@@ -52,10 +53,25 @@ namespace CardGames.GameLogic
 		/// Returns all of the cards to the Deck, and shuffles their order.
 		/// All cards are turned so that they are face down.
 		/// </summary>
-		public void Shuffle()
-		{
-			//TODO: implement shuffle!
-		}
+        public void Shuffle()
+        {
+            for (int i = 0; i < 52; i++)
+            {
+                if (_cards[i].FaceUp) _cards[i].TurnOver();
+            }
+            Random rnd = new Random();
+            // for each card (no need to shuffle last card)
+            for (int i = 0; i < 52 - 1; i++)
+            {
+                // pick a random index
+                int rndIdx = rnd.Next(52 - i);
+                Card temp = _cards[i];
+                _cards[i] = _cards[i + rndIdx];
+                _cards[i + rndIdx] = temp;
+            }
+            _topCard = 0;
+        }
+    
         
 		/// <summary>
 		/// Takes a card from the top of the Deck. This will return
